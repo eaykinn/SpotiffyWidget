313a98da2cc21370799bbdf64b4806f452f13541
emre aykın <eaykin61@gmail.com>
2025-10-28 20:25:41 +0300

    dt helper

---
+++ Helpers/DesktopHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+public static class DesktopHelper
+{
+    [DllImport("user32.dll")]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+    public static void SetAsDesktopChild(Window window)
+    {
+        IntPtr hWnd = new WindowInteropHelper(window).Handle;
+        IntPtr hProgman = FindWindow("Progman", null);
+        SetParent(hWnd, hProgman);
+    }
+}
